870cc3bc56ee80f5a374a912fb61837087b5e41f
Arin Houck <arinhouck@gmail.com>
2015-11-16 12:45:56 -0700

    Initial Commit

---
+++ lab9Tests/lab9Tests.swift
@@ -0,0 +1,36 @@
+//
+//  lab9Tests.swift
+//  lab9Tests
+//
+//  Created by Arin Houck on 11/16/15.
+//  Copyright © 2015 Arin Houck. All rights reserved.
+//
+
+import XCTest
+@testable import lab9
+
+class lab9Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
